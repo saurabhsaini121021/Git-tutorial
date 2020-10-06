cbac48f4f9243c44ddb897a190aa66f50eb603bd
Saurabh <sainisaurabh121021@gmail.com>
2020-10-06 17:02:41 +0530

    features initial commit

--- sum/src/java1.java
+++ sum/src/java1.java
@@ -1,7 +1,7 @@
 public class java1 {
     public static void main(String args[]){
-        int x= 5;
-        int y =10;
+        int x= 50;
+        int y =100;
         int sum = x+y;
         System.out.println("the sum of x and y is = "+ sum);
     }
