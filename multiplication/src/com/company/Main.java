00187b8123b4b92584b3b8597c1eb451b175acea
Saurabh <sainisaurabh121021@gmail.com>
2020-10-06 16:55:27 +0530

    intial commit

---
+++ multiplication/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int x= 5;
+        int y =10;
+        int multi = x*y;
+        System.out.println("the multiplication of x and y is = "+ multi);
+    }
+}
---
+++ sum/src/java1.java
@@ -0,0 +1,8 @@
+public class java1 {
+    public static void main(String args[]){
+        int x= 5;
+        int y =10;
+        int sum = x+y;
+        System.out.println("the sum of x and y is = "+ sum);
+    }
+}
